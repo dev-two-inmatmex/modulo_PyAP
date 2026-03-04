a098aed7fc332a7629e373f1f89dd1cef9a0ad9b
Developer Two <devtwo.inmatmex@gmail.com>
2026-03-04 22:26:24 +0000

    ajuste de umbral biometrico

--- src/app/(shared)/checador/actions.ts
+++ src/app/(shared)/checador/actions.ts
@@ -82,7 +82,7 @@ export async function registrarChequeo(
     
         console.log('distancia biometrica:', biometricResult);
         const score = biometricResult as number;
-        if (biometricResult > 0.38) { // Umbral para distancia de Coseno
+        if (biometricResult > 0.55) { // Umbral para distancia de Coseno
             return { error: `Identidad no verificada. (Score: ${score.toFixed(4)})` };
         }
 
