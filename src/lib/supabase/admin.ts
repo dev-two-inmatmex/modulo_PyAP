b175c47a23d347c9236ac386ef9c7baab0acc8df
Developer Two <devtwo.inmatmex@gmail.com>
2026-03-10 22:08:40 +0000

    ajuste de admin.ts

--- src/lib/supabase/admin.ts
+++ src/lib/supabase/admin.ts
@@ -3,6 +3,13 @@ import { createClient } from '@supabase/supabase-js'
 export const createAdminClient = () => {
   return createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.SUPABASE_SERVICE_ROLE_KEY! // Clave privada
+    process.env.SUPABASE_SERVICE_ROLE_KEY!, // Clave privada
+    {
+      auth: {
+        autoRefreshToken: false,
+        persistSession: false,
+        detectSessionInUrl: false
+      }
+    }
   )
 }
